974f76239f5304234885026eef29dc9aedb131fa
Valentin08325 <vali.stefanov.08325@gmail.com>
2025-09-07 10:32:57 +0300

    Add project files.

---
+++ Bank.test/BanksTesting.cs
@@ -0,0 +1,51 @@
+using Bank;
+using NUnit.Framework;
+
+namespace Bank.test
+{
+    public class BankTests
+    {
+        [Test]
+        public void Deposit_PositiveAmount_IncreasesBalance()
+        {
+            // Arrange
+            var bank = new Banks(12345);
+            int initialBalance = bank.Balance;
+            int deposit = 100;
+            // Act
+            bank.Deposit(deposit);
+            //Assert
+            Assert.AreEqual(initialBalance + deposit, bank.Balance);
+        }
+        [Test]
+        public void Deposit_NegativeAmount_ThrowsException()
+        {
+            //Arrange
+            var bank = new Banks(12345);
+            int deposit = -50;
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() => bank.Deposit(deposit));
+        }
+        [Test]
+        public void Withdraw_ValidAmount_DecreasesBalance()
+        {
+            //Arrange
+            var bank = new Banks(12345);
+            int initialBalance = bank.Balance;
+            int withdraw = 12;
+            //Act
+            bank.Withdraw(withdraw);
+            //Assert
+            Assert.AreEqual(initialBalance - withdraw, bank.Balance);
+        }
+        [Test]
+        public void Withdraw_InvalidAmount_ThrowsExcpetion()
+        {
+            //Arrange
+            var bank = new Banks(12345);
+            int withdraw = -20;
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() => bank.Withdraw(withdraw));
+        }
+    }
+}
---
+++ Bank/Banks.cs
@@ -0,0 +1,47 @@
+﻿public class Banks
+{
+    private int balance;
+    private int accountNumber;
+
+    public Banks(int accountNumber)
+    {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public int Balance
+    {
+        get { return balance; }
+    }
+
+    public int AccountNumber
+    {
+        get { return accountNumber; }
+    }
+
+    public void Deposit(int amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+        else
+        {
+            throw new InvalidOperationException("Deposit amount must be positive.");
+        }
+    }
+
+    public void Withdraw(int amount)
+    {
+        if (amount == 0 && amount <= balance)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid withdrawal amount.");
+        }
+    }
+
+    public int GetBalance() { return balance; }
+}
